911d73296a70dc864c26fdaf13e40cc2e08fc0d9
Pedrohumberto01 <pcolindres8@gmail.com>
2025-09-06 15:18:18 -0600

    retoques de algunas cositas en las pantallas de login

--- salud_app_mobile/lib/presentation/widgets/login/register_text.dart
+++ salud_app_mobile/lib/presentation/widgets/login/register_text.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../screens/login/register_screen.dart';
+
 class RegisterText extends StatelessWidget {
   const RegisterText({super.key});
 
@@ -10,12 +12,18 @@ class RegisterText extends StatelessWidget {
       children: [
         const Text("¿No tienes una cuenta? "),
         GestureDetector(
-          onTap: () {},
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const RegisterScreen()),
+            );
+          },
           child: const Text(
             "Regístrate",
             style: TextStyle(
               color: Color(0xFF1c1ec5),
               fontWeight: FontWeight.bold,
+              decoration: TextDecoration.underline,
             ),
           ),
         ),
