4a9f8af9b6b93cfb2ba70e859d355afd0ee3dfdc
Francisco Paredes <fparedes.dev@outlook.com>
2025-09-12 10:28:38 -0600

    Se agregaron nuevos endpoints getter a utilidades

--- SaludAppBackend/SaludAppBackend.Services/UtilitiesService/UtilitiesService.cs
+++ SaludAppBackend/SaludAppBackend.Services/UtilitiesService/UtilitiesService.cs
@@ -20,9 +20,43 @@ namespace SaludAppBackend.Services.UtilitiesService
             _logger = logger;
         }
 
+        public async Task<IEnumerable<TbAreasMedica>> GetAreasMedicasAsync()
+            => await _unitOfWork.Utilities.GetAreasMedicasAsync();
+
+        public async Task<IEnumerable<TbBarrio>> GetBarriosAsync()
+            => await _unitOfWork.Utilities.GetBarriosAsync();
+
+        public async Task<IEnumerable<TbCentrosMedico>> GetCentrosMedicosAsync()
+            => await _unitOfWork.Utilities.GetCentrosMedicosAsync();
+
+        public async Task<IEnumerable<TbDepartamento>> GetDepartamentosAsync()
+            => await _unitOfWork.Utilities.GetDepartamentosAsync();
+
+        public async Task<IEnumerable<TbEspecialidade>> GetEspecialidadesMedicasAsync()
+            => await _unitOfWork.Utilities.GetEspecialidadesMedicasAsync();
+
+        public async Task<IEnumerable<TbGenero>> GetGenerosAsync()
+            => await _unitOfWork.Utilities.GetGenerosAsync();
+
+        public async Task<IEnumerable<TbMunicipio>> GetMunicipiosAsync()
+            => await _unitOfWork.Utilities.GetMunicipiosAsync();
+
+        public async Task<IEnumerable<TbOcupacione>> GetOcupacionesPacientesAsync()
+            => await _unitOfWork.Utilities.GetOcupacionesPacientesAsync();
+
+        public async Task<IEnumerable<TbProvTelefonico>> GetProveedoresTelefonicoAsync()
+            => await _unitOfWork.Utilities.GetProveedoresTelefonicoAsync();
+
+        public async Task<IEnumerable<TbReligione>> GetReligionesAsync()
+            => await _unitOfWork.Utilities.GetReligionesAsync();
+
         public async Task<IEnumerable<TbTipoUsuario>> GetTiposDeUsuario()
-        {
-            return await _unitOfWork.Utilities.GetTiposDeUsuarioAsync();
-        }
+            => await _unitOfWork.Utilities.GetTiposDeUsuarioAsync();
+
+        public async Task<IEnumerable<TbTurnosMedico>> GetTurnosMedicosAsync()
+            => await _unitOfWork.Utilities.GetTurnosMedicosAsync();
+
+        public async Task<IEnumerable<TbUniversidade>> GetUniversidadesAsync()
+            => await _unitOfWork.Utilities.GetUniversidadesAsync();
     }
 }
