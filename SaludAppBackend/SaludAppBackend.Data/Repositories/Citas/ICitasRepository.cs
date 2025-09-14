ed9fb4852cfd4994620751b7cf02e8be6457cfd0
Francisco Paredes <fparedes.dev@outlook.com>
2025-09-13 22:58:14 -0600

    Se agregan tablas para manejo de citas

---
+++ SaludAppBackend/SaludAppBackend.Data/Repositories/Citas/ICitasRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaludAppBackend.Data.Repositories.Citas
+{
+    public interface ICitasRepository
+    {
+        Task AddCitaMedica();
+        Task AddCitaLaboratorio();
+    }
+}
